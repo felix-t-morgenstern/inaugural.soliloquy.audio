95acf9c216080372efe6d31a67109ba36a36f5d7
felix.t.morgenstern <felix.t.morgenstern@gmail.com>
2019-02-16 23:09:51 -0500

    Created Audio
    Created Audio. (No unit tests implemented, since the class has no logic to speak of.)

---
+++ src/inaugural/soliloquy/audio/Audio.java
@@ -0,0 +1,24 @@
+package inaugural.soliloquy.audio;
+
+import soliloquy.audio.specs.IAudio;
+import soliloquy.audio.specs.ISoundFactory;
+import soliloquy.audio.specs.ISoundsPlaying;
+
+public class Audio implements IAudio {
+	private final ISoundsPlaying SOUNDS_PLAYING;
+	private final ISoundFactory SOUND_FACTORY;
+	
+	public Audio(ISoundsPlaying soundsPlaying, ISoundFactory soundFactory)
+	{
+		SOUNDS_PLAYING = soundsPlaying;
+		SOUND_FACTORY = soundFactory;
+	}
+
+	public ISoundsPlaying soundsPlaying() {
+		return SOUNDS_PLAYING;
+	}
+
+	public ISoundFactory soundsLibrary() {
+		return SOUND_FACTORY;
+	}
+}
